ddd544f8a40ffbebee9b2d7f559c8f7bd87ce723
ysyysyysy-502 <nasrullah112@madrasah.kemenag.go.id>
2026-05-28 01:15:48 +0800

    Delete cyberdog_race_2026_6 directory

--- cyberdog_race_2026_6/src/cyberdog_controller/FSM/states/__init__.py
+++
@@ -1,3 +0,0 @@
-from .state import *
-from .stage6 import *
-from .basic_state import *
--- cyberdog_race_2026_6/src/cyberdog_controller/camera/__init__.py
+++
@@ -1,6 +0,0 @@
-from .path_scanner import (
-    FootballScanner, FinishCircleScanner, CenterOffsetScanner,
-    LidarObstacleScanner, PathScannerBase,
-    check_camera_ready, path_scanner_main,
-    football_scanner_main, finish_circle_scanner_main, lidar_scanner_main
-)
